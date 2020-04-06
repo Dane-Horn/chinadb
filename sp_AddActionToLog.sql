6c1692f2c72df9b7d7ca5a4f0a9adda56d3aa1c0
Dane Horn <dane.horn7915@gmail.com>
2020-04-06 11:27:09 +0200

    use citizen and actionslog insert stored procedures for inserts. Change actionslog stored procedure name to use usp_ prefix

--- sp_AddActionToLog.sql
+++ sp_AddActionToLog.sql
@@ -1,10 +1,10 @@
 use master;
 use SurveillenceDB;
 GO
-DROP PROCEDURE [dbo].AddActionToLog;  
+DROP PROCEDURE [dbo].usp_AddActionToLog;  
 GO  
 
-CREATE PROCEDURE [dbo].AddActionToLog  
+CREATE PROCEDURE [dbo].usp_AddActionToLog  
     @citizenId bigint,   
     @actionId int,
 	@cameraId int,
@@ -27,7 +27,4 @@ SET score = score + @actionScore
 WHERE @citizenId = [Citizens].citizenId
 
 END
-GO
-
-EXEC [dbo].AddActionToLog @citizenId= 1009811972509, @actionId = 2, @cameraId = 2, @accuracy = 98, @occurenceTime = '2016-12-21 00:00:00.000';
 GO
