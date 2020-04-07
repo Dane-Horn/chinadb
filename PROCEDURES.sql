9f8142c93e0b2c29a6cda3312b16f704eeed24be
Ianlabusch <62617411+Ianlabusch@users.noreply.github.com>
2020-04-07 09:06:25 +0200

    Update PROCEDURES.sql

--- PROCEDURES.sql
+++ PROCEDURES.sql
@@ -12,12 +12,13 @@ CREATE PROCEDURE [dbo].usp_insertCitizen
 @pGender varchar(25),
 @pDistrictID int,
 @pOccupationID int,
+@pMarkerID int,
 @pAddressLine1 varchar(200),
 @pAddressLine2 varchar(200),
 @pScore float
 AS
-INSERT INTO [dbo].Citizens (citizenId, firstName, lastName, dateOfBirth, gender, districtId, occupationId, addressLine1, addressLine2,score)
-    VALUES (@pCitizenID,@pFirstName,@pLastName,@pDOB,@pGender,@pDistrictID,@pOccupationID,@pAddressLine1,@pAddressLine2,@pScore)
+INSERT INTO [dbo].Citizens (citizenId, firstName, lastName, dateOfBirth, gender, districtId, occupationId, markerId, addressLine1, addressLine2,score)
+    VALUES (@pCitizenID,@pFirstName,@pLastName,@pDOB,@pGender,@pDistrictID,@pOccupationID,@pMarkerID,@pAddressLine1,@pAddressLine2,@pScore)
 GO
 
 DROP PROCEDURE [dbo].usp_AddActionToLog;  
