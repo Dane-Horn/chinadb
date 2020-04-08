98974c3960f77f6ad9e817f83b0149188a499b46
jonathanc <jonathanc@bbd.co.za>
2020-04-08 17:29:26 +0200

    Added Permissions to Roles (still need table permissions)

--- Roles.sql
+++ Roles.sql
@@ -29,9 +29,6 @@ create role Government
 drop role Police
 create role Police
 
-drop role PopulationControl
-create role PopulationControl
-
 drop role SuperUser
 create role SuperUser
 
@@ -47,4 +44,33 @@ add member ElPresidente
 
 grant control on DATABASE::SurveillenceDB to GodUser
 
+
+------Permissions->USP------
+
+grant execute on usp_AddActionToLog to Police
+grant execute on usp_insertCitizen to Government
+grant execute on usp_MarkCitizen to SuperUser
+
+------Permissions->Tables------
+--need to add other CRUD operations either here or in USP
+grant select, insert, update, delete  on schema :: dbo to SuperUser
+
+------Permissions->Views------
+
+grant select on vGendersPerOccupation to Government
+grant select on vPopulationPerDistrict to Government
+grant select on vScorePerDistrict to Government
+grant select on vScorePerOccupation to Government
+
+grant select on vActionsPerOccupation to Police
+grant select on vCitizensActions to Police
+grant select on vMostActionsPerCamera to Police
+
+------Permissions->Functions------
 grant select on fCamerasToMaintain to Government
+grant select on fMostActionsBetweenDates to Government
+
+
+
+
+
